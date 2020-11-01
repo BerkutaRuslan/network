8d49b1f0dd4c7bea74539f03179d422380927dad
Ruslan <berkutruslan1@gmail.com>
2020-11-01 18:44:36 +0200

    added like/unlike endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from posts.views import CreatePostView
+from posts.views import CreatePostView, AddLikeView, RemoveLikeView
 
 urlpatterns = [
     path('create', CreatePostView.as_view()),
-    # path('<int:pk>/like', AddLikeView.as_view()),
-    # path('<int:pk>/unlike'),
+    path('<int:pk>/like', AddLikeView.as_view()),
+    path('<int:pk>/unlike', RemoveLikeView.as_view()),
     # path('analytics'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from posts.models import Likes, Post
+from posts.serializers import CreatePostSerializer
+
+
+class CreatePostView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = CreatePostSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({"message": "Post was created!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AddLikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = Post.objects.get(id=pk)
+        user = request.user
+        if Likes.objects.filter(user=request.user, post=post).exists():
+            return Response({'message': 'You already liked this post'}, status=status.HTTP_200_OK)
+        else:
+            Likes.objects.create(user=user, post=post)
+            return Response({'message': 'You have liked this post'}, status=status.HTTP_200_OK)
+
+
+class RemoveLikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = Post.objects.get(id=pk)
+        try:
+            Likes.objects.get(user=request.user, post=post).delete()
+            return Response({'message': 'Removed like from this post'}, status=status.HTTP_200_OK)
+        except Likes.DoesNotExist:
+            return Response({'message': "You didn't like this post"}, status=status.HTTP_200_OK)
