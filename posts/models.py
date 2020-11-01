32e717c86bca33a097bd79e138e41c8a956881e4
Ruslan <berkutruslan1@gmail.com>
2020-11-01 18:12:08 +0200

    added post and like models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField(max_length=2000)
+    user = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, related_name='post_likes', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='likes', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
